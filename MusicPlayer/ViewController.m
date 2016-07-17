350774464403ccdf6c6738b506ffd8b721fece39
Bean He <withhebing@gmail.com>
2016-07-17 20:51:13 +0800

    新建工程

---
+++ MusicPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MusicPlayer
+//
+//  Created by Beans on 16/7/17.
+//  Copyright © 2016年 iceWorks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
