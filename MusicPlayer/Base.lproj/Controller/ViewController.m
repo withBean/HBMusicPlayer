9889d420d2b9c54688e28272ce9e0eff84417d02
Bean He <withhebing@gmail.com>
2016-07-18 14:01:28 +0800

    属性连线 横竖界面分开, 共有属性写一起 注意: @property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *lyricLbls;

--- MusicPlayer/Base.lproj/Controller/ViewController.m
+++ MusicPlayer/Base.lproj/Controller/ViewController.m
@@ -9,6 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+#pragma mark - H&V
+@property (weak, nonatomic) IBOutlet UIImageView *bgImageView;
+@property (weak, nonatomic) IBOutlet UILabel *currentTimeLbl;
+@property (weak, nonatomic) IBOutlet UILabel *durationLbl;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *lyricLbls;
+
+#pragma mark - H
+@property (weak, nonatomic) IBOutlet UIView *hCenterView;
+@property (weak, nonatomic) IBOutlet UIImageView *hSingerIcon;
+
+#pragma mark - V
+@property (weak, nonatomic) IBOutlet UIView *vCenterView;
+@property (weak, nonatomic) IBOutlet UIImageView *vSingerIcon;
+@property (weak, nonatomic) IBOutlet UILabel *singerLbl;
+@property (weak, nonatomic) IBOutlet UILabel *albumLbl;
+
+#pragma mark - 私有属性
 
 @end
 
@@ -22,6 +41,18 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)play {
+}
+
+- (IBAction)previous {
+}
+
+- (IBAction)next {
+}
+
+- (IBAction)sliderValueChange {
 }
 
 @end
